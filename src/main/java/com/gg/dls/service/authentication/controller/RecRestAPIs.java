561ba0496c3a47c494d967732642efa7e629f0fb
Petya Buchkova <petya.buchkova@gmail.com>
2019-05-30 17:29:36 +0200

    Changed to my db username and password

--- src/main/java/com/gg/dls/service/authentication/controller/RecRestAPIs.java
+++ src/main/java/com/gg/dls/service/authentication/controller/RecRestAPIs.java
@@ -4,16 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import com.gg.dls.service.authentication.message.response.RecommendationResponse;
 
 import io.jsonwebtoken.Jwts;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/rec")
 public class RecRestAPIs {
